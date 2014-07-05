fa7149ead439dc55f6ea8e982e7dfe06a7e2edbd
affanmalik <amalik@sfu.ca>
2014-07-04 20:06:52 -0700

    Bug fix

--- novella/lms/urls.py
+++ novella/lms/urls.py
@@ -4,8 +4,8 @@ from lms import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', view.auth_and_login, name='login'),
-    url(r'^register/$', view.sign_up_in, name='login'),
+    url(r'^login/$', views.auth_and_login, name='login'),
+    url(r'^register/$', views.sign_up_in, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^loginform/$', views.loginform, name='weblogin'),
 
