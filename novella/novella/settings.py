09b7e0f440cbfd263e1c54c323288b74fc46f610
affanmalik <amalik@sfu.ca>
2014-07-06 18:15:44 -0700

    Added tasty-pie. Also added a CORS app to enable CORS requests

--- novella/novella/settings.py
+++ novella/novella/settings.py
@@ -36,11 +36,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'south','lms',
+	'south','lms', 'tastypie', 'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -84,3 +85,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ CORS_ORIGIN_ALLOW_ALL = True
