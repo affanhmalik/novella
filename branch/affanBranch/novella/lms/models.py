8a83b250f2c816fe91911cfa716041432d1104a2
chrisNiu <cniusfu2013@gmail.com>
2014-07-08 19:59:50 -0700

    UPDATE: Re-organize the structure of repo

---
+++ branch/affanBranch/novella/lms/models.py
@@ -0,0 +1,27 @@
+#models.py for the Novella LMS API
+#Programmers: Affan, Sen
+#
+#
+#
+
+
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#This will be the main student model. It has a one-to-one relationship with the built-in django user class
+class Student(models.Model):
+	studentid = models.ForeignKey(User)
+	first_name = models.CharField(max_length=60)
+	last_name = models.CharField(max_length=60)
+	email = models.CharField(max_length=60)
+
+class Instructor(models.Model):
+	instid = models.ForeignKey(User)
+	first_name = models.CharField(max_length=60)
+	last_name = models.CharField(max_length=60)
+
+
---
+++ branch/affanBranch/novella/lms/urls.py
@@ -0,0 +1,23 @@
+#urls.py for the Novella LMS API
+#Programmers: Affan, Danhui, Ying
+#
+#
+#
+
+from django.conf.urls import patterns, url, include
+#from django.conf.urls.defaults import *
+from lms.api import StudentResource
+
+from lms import views
+
+student_resource = StudentResource()
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.auth_and_login, name='login'),
+    url(r'^register/$', views.sign_up_in, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^loginform/$', views.loginform, name='weblogin'),
+    url(r'^students/', include(student_resource.urls)),
+
+)
