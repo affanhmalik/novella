cecd5697c23fe2fb5be4b541012dba14ac61c51e
Djangoubu <affanhmalik@gmail.com>
2014-07-12 01:57:20 -0700

    Fixed: Import student and intructor model

--- branch/affanBranch/novella/lms/views.py
+++ branch/affanBranch/novella/lms/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User, Student, Instructor
+from django.contrib.auth.models import User
 import json
 from django.http import HttpResponse
 
