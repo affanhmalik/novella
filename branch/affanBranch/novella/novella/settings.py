c6537ba230cc3901a461c9edb2c55a3a5643d18a
yca158 <yca158@sfu.ca>
2014-07-24 23:22:00 -0700

    Add background to the admin login page
    Change background of the login page and change the admin-site’s name.

--- branch/affanBranch/novella/novella/settings.py
+++ branch/affanBranch/novella/novella/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 
 # Quick-start development settings - unsuitable for production
@@ -97,3 +98,6 @@ AUTH_PROFILE_MODULE = 'lms.UserProfile'
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
 
 MEDIA_URL = '/media/'
+
+
+
