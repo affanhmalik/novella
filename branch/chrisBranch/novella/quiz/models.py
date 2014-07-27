eaca5f1e74f77ce4d2fe60f521832ce3d3d4898c
chrisNiu <cniusfu2013@gmail.com>
2014-07-27 12:33:20 -0700

    UPDATE: quiz API

--- branch/chrisBranch/novella/quiz/models.py
+++ branch/chrisBranch/novella/quiz/models.py
@@ -8,25 +8,33 @@ import lms, course
 class Quiz(models.Model):
 	student = models.ForeignKey(lms.models.Student, related_name='quiz_student')
 	course = models.ForeignKey(course.models.Section,related_name = 'quiz_coursesection') 
-	quizName = models.CharField(max_length=200)
-	introduction = models.TextField(max_length=800)
+	name = models.CharField(max_length=200)
+	# introduction = models.TextField(max_length=800)
 	timeLimit = models.IntegerField(default=5)
-	grade = models.FloatField(default=0)
+	dueDate = models.DateField()
+	timeDue = models.TimeField()
+	# grade = models.FloatField(default=0)
 
 	def __unicode__(self):
-		return self.quizName
+		return self.name
 
 
 class Question(models.Model):
 	quiz = models.ForeignKey('Quiz', related_name = 'questions')
-	text = models.TextField(max_length=800)
+	value = models.TextField(max_length=800)
 	choiceA = models.CharField(max_length=200)
 	choiceB = models.CharField(max_length=200)
-	choiceC = models.CharField(max_length=200)
-	choiceD = models.CharField(max_length=200)
-	corrAns = models.CharField(max_length=50)
-	response = models.CharField(max_length=50, default=None, blank=True)
+	choiceC = models.CharField(max_length=200, blank=True)
+	choiceD = models.CharField(max_length=200, blank=True)
+	answer = models.CharField(max_length=50)
+	# response = models.CharField(max_length=50, default=None, blank=True)
 
 	def __unicode__(self):
-		return self.text
+		return self.value
 
+# class Choice(models.Model):
+# 	question = models.OneToOneField('Question', related_name = 'question_choice')
+# 	choiceA = models.CharField(max_length=200)
+# 	choiceB = models.CharField(max_length=200)
+# 	choiceC = models.CharField(max_length=200, blank=True)
+# 	choiceD = models.CharField(max_length=200, blank=True)
