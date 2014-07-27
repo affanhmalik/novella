580cb07d52b57ed771ffe9c0d97541f6f63743ef
Djangoubu <affanhmalik@gmail.com>
2014-07-27 06:07:48 -0700

    Fixing register

--- branch/affanBranch/novella/lms/urls.py
+++ branch/affanBranch/novella/lms/urls.py
@@ -16,8 +16,8 @@ from lms import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login/$', csrf_exempt(views.auth_and_login), name='login'),
-    url(r'^register/$', csrf_exempt(views.testlogin), name='login'),
-    #url(r'^register/$', views.sign_up_in, name='login'),
+    #url(r'^register/$', csrf_exempt(views.testlogin), name='login'),
+    url(r'^register/$', views.sign_up_in, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^loginform/$', views.loginform, name='weblogin'),
 
