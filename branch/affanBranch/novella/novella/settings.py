ff58ac81a9be84e4fd8dc231a08556640b3e3ea1
Djangoubu <affanhmalik@gmail.com>
2014-07-27 05:29:35 -0700

    fixed settings.py

--- branch/affanBranch/novella/novella/settings.py
+++ branch/affanBranch/novella/novella/settings.py
@@ -84,16 +84,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'novella/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_EXPOSE_HEADERS = ()
 
-ADMIN_MEDIA_PREFIX = '/admin-media/'
+ADMIN_MEDIA_PREFIX = 'novella/admin-media/'
 
 AUTH_PROFILE_MODULE = 'lms.UserProfile'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
 
 MEDIA_URL = '/media/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
