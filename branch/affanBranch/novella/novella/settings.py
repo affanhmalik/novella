9beba5e0c21d26634b1dcda3e08a477f0fe166bd
Djangoubu <affanhmalik@gmail.com>
2014-07-27 05:32:27 -0700

    fixed settings.py

--- branch/affanBranch/novella/novella/settings.py
+++ branch/affanBranch/novella/novella/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -98,4 +98,3 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
 
 MEDIA_URL = '/media/'
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
